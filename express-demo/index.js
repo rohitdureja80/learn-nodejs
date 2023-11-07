069e2a3032452d408b3a46bf94fb8b9a70e8b77f
Rohit Dureja <rdureja@gmail.com>
2023-11-07 13:46:54 -0500

    added handling get requests

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,13 +2,27 @@
 const express = require('express');
 const app = express();
 
-const courses = [1, 2, 3, 4];
+const courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"}
+]
 app.get('/', (req, res) => {
     res.send("Hello World");
 });
 
 app.get('/api/courses', (req, res) => {
     res.send(courses);
-})
+}); 
+
+app.get('/api/courses/:id', (req, res) => {
+    //res.send(`course id: ${req.params.id}`);
+    //res.send(req.query); // show all query parameters
+    //res.send(req.params); // show all path parameters 
+    const course = courses.find(x => x.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given id was not found.');
+    res.send(course);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
